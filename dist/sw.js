eff39f2e5582db5ada3350d63392cd99491eda6d
Tim McMaster <tim@mcmaster.id.au>
2019-05-16 13:55:33 +1000

    checkpoint

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.f3c44cb8b78a369fb403278e435af1b3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.f5b78ac7021a7d1a9f8fb3faa3a335d0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
