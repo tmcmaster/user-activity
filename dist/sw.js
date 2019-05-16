4590fd71ddf329b851ae6c441ffea0be79e5e4f8
Tim McMaster <tim@mcmaster.id.au>
2019-05-16 11:36:42 +1000

    init

---
+++ dist/sw.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.104a37f4e9d083f4f37c01706592d800.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+
---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('Starting User Activity Visualiser.');
